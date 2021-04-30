512d1478b56aafa3418ec634e1ea7b95b492fe57
TonitoGueli <62585975+TonitoGueli@users.noreply.github.com>
2021-04-30 16:35:11 -0400

    VER 0.0
    En este upload, se sube el esqueleto del proyecto.

---
+++ CreadorDeCurriculums/src/main/java/com/example/CreadorDeCurriculums/Modelo/Curriculums.java
@@ -0,0 +1,4 @@
+package com.example.CreadorDeCurriculums.Modelo;
+
+public class Curriculums {
+}
---
+++ CreadorDeCurriculums/src/test/java/com/example/CreadorDeCurriculums/CreadorDeCurriculumsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CreadorDeCurriculums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreadorDeCurriculumsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
