f3c733840232de04a3ecf30466256add560988fa
TonitoGueli <62585975+TonitoGueli@users.noreply.github.com>
2021-05-04 17:11:48 -0400

    Add files via upload

--- CurriculumWebApp/src/main/java/com/tonito/curriculumwebapp/CurriculumWebAppApplication.java
+++ CurriculumWebApp/src/main/java/com/tonito/curriculumwebapp/CurriculumWebAppApplication.java
@@ -2,7 +2,6 @@ package com.tonito.curriculumwebapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 public class CurriculumWebAppApplication {
